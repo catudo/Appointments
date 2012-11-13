cc995946352e2103d77d96381e44bd3918e4b787
cancerbero <cancerbero@cancerbero-PC>
2012-11-12 22:28:51 -0500

    much more

---
+++ app/webroot/js/i18n/messages.en.js
@@ -0,0 +1,13 @@
+var LK = {};
+LK['atrributes.required'] = 'the [] are required';
+LK['atrribute.required'] = 'the [] is required';
+LK['confirm.password'] = 'Confirm password is required';
+LK['confirm.password.equals'] = 'Password and confirm password fields are not equals';
+LK['singular.category'] = "a Category"
+LK['singular.exhibition'] = "an Exhibition"
+LK['singular.language'] = "a Language"
+LK['sZeroRecords'] = "No data"
+LK['model.constraint'] = "at least must exist []";
+LK['uploading.label'] = "Uploading...";
+LK['uploading.complete'] = "Upload Complete";
+
---
+++ app/webroot/js/users.js
@@ -0,0 +1,80 @@
+$(function(){
+
+listUsers()
+saveUser()
+edit()
+})
+
+
+function listUsers(){
+    $.ajax({
+        url: webroot + "/users/listUsers",
+        type: 'POST',
+        success: function(response){
+        		//var response = jQuery.parseJSON(r)
+				buildTable(response,"userTable","#userTableDiv")
+				 
+        }
+    });
+  
+}
+
+
+
+function edit(){
+	 $("#containerHolder").delegate(".edit", "click", function(event) {
+	 event.preventDefault()
+	 $("#password").attr('disabled',true);
+     $("#secondPassword").attr('disabled',true);
+	 $("#editPassword").show();
+	 
+	 var params = {userId: $(this).attr('userId') };
+		 $.ajax({
+    	    url: webroot + "/users/edit",
+    	    type: 'POST',
+			data:params,
+       	 	success: function(response){
+        			
+					$.fillthis(response);
+
+    	    }
+	    });
+	});
+	
+	$("#content").delegate("#editPassword", "click", function(event) {
+		$("#password").attr('disabled',false);
+     	$("#secondPassword").attr('disabled',false);
+	});
+
+
+}
+
+function saveUser(){
+	$("#containerHolder").delegate("#saveUserButton", "click", function(event) {
+		
+		event.preventDefault();
+		var params = $("#saveUserForm").serialize()
+		$.ajax({
+    	    url: webroot + "/users/save",
+    	    type: 'POST',
+			data:params,
+       	 	success: function(r){
+        			if(r.errors !=undefined){
+        				showErrors(r.errors);
+        			}else{
+        				clear("#saveUserForm");	
+        				listUsers()
+        				
+        				
+        			}
+    	    }
+	    });
+	    });
+	    
+	    		
+}
+
+
+
+
+
